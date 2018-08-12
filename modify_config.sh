fe32e35899ae1259575795a6866ba6a9de7c893a
corsaro1 <13351534+corsaro1@users.noreply.github.com>
2018-08-12 10:48:20 +0200

    Create modify_config.sh

---
+++ modify_config.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+/*
+setup:
+install jq:
+sudo apt-get install jq
+
+manually modify "encryptedPassphrase" and "publicKey" accordling with your configuration
+
+usage:
+bash modify_config2.sh
+*/
+
+configfile='config.json'
+cp $configfile "$configfile-original"
+external_ip='"2.2.2.2"'
+
+
+DELEGATES='[{"encryptedPassphrase": "salt=*********&cipherText=*************&iv=**********&tag=********&version=1","publicKey": "abf9787621f8f43ec4e4a645b515094f42fc5615f2e231eca24eaf6e69dc6a65"}]'
+
+jq ".forging.delegates = $DELEGATES" "$configfile-original" > config.json
+
+
+SSL=' {
+      "enabled": true,
+      "options": {
+        "port": 2443,
+        "address": "0.0.0.0",
+        "key": "./ssl/lisk.key",
+        "cert": "./ssl/lisk.crt"
+      }
+    }'
+cp $configfile "$configfile-temp"
+jq ".api.ssl = $SSL" "$configfile-temp" > config.json
+
+
+API='{
+      "public": false,
+      "whiteList": [
+        "127.0.0.1",'$external_ip'
+      ]
+    }'
+cp $configfile "$configfile-temp"
+jq ".api.access = $API" "$configfile-temp" > config.json
+    
+WHITELIST=' [
+        "127.0.0.1",
+        '$external_ip'
+      ]
+'
+cp $configfile "$configfile-temp"
+jq ".forging.access.whiteList = $WHITELIST" "$configfile-temp" > config.json

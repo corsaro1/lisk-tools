00f1ca47c7fe0ce675f2fb84779547fc0ea8cc9d
corsaro1 <13351534+corsaro1@users.noreply.github.com>
2018-09-25 06:02:05 +0200

    Update alert_missed_block.sh

--- alert_missed_block.sh
+++ alert_missed_block.sh
@@ -8,28 +8,34 @@
 
 delegate_address="3026381832248350807L"
 your_email="email@domain.tld"
+telegram_contact="xx_yy"
 
-missedblocks=$(curl -s "https://node01.lisk.io/api/delegates?address=$delegate_address" | jq .data[0].missedBlocks)
-echo $missedblocks
+s1=$(curl -s "https://node01.lisk.io/api/delegates?address=$delegate_address" | jq .data[0].missedBlocks)
+echo $s1
 
 while true; do
 
-    s1=$(curl -s "https://node01.lisk.io/api/delegates?address=$delegate_address" | jq .data[0].missedBlocks)
-    sleep 60
+	sleep 60
     s2=$(curl -s "https://node01.lisk.io/api/delegates?address=$delegate_address" | jq .data[0].missedBlocks)
     
 
     if [ "$s1" == "$s2" ]
-   then
+		then
           
         sleep 10
 	
     else
      
-      echo "you missed a block"
-      mail -s "block missed" $your_email < /dev/null
-	sleep 10
-fi
+		echo "alert you missed a block"
+		mail -s "block missed" $your_email < /dev/null
+		/snap/bin/telegram-cli -W -e "msg $telegram_contact you missed a new block. You are at $s2 missed now"
+		sleep 10
+		s1=$(curl -s "https://node01.lisk.io/api/delegates?address=$delegate_address" | jq .data[0].missedBlocks)
+		echo $s1
+
+
+	fi
 
 done
+
 
